370765c0c04de46964972d90ae88b3e7835e21ec
Zachary <Zacharygking@g.ucla.edu>
2015-08-27 11:23:55 -0700

    Model Update

--- report/admin.py
+++ report/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Report
+from .models import Campaign, AdGroup, Keyword
 
-admin.site.register(Report)
+admin.site.register(Campaign)
+admin.site.register(AdGroup)
+admin.site.register(Keyword)
 # Register your models here.
