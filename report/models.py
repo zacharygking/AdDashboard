2524399d92afe2e20b00247fb9a9b7f3564b0da8
bluej-21 <jys023@ucsd.edu>
2015-08-27 14:35:57 -0700

    Update models.py

--- report/models.py
+++ report/models.py
@@ -7,30 +7,27 @@ class Account(models.Model):
 
     def __str__(self):
         return self.account_name
-        
-class Keyword(models.Model):
-    keyword_id = models.IntegerField(default=0)
-    keyword_placement = models.CharField(max_length=200,default='')
-    clicks = models.IntegerField(default = 0)
-    impressions = models.IntegerField(default = 0)
-    cost = models.IntegerField(default = 0)
-    
-    def __str__(self):
-        return self.keyword_placement
-        
-class AdGroup(models.Model):
-    ad_group_name = models.CharField(max_length=200,default='')
-    campaign_name = models.CharField(max_length=200,default='')
-    
-    keyword = models.ForeignKey(Keyword)
-    
-    def __str__(self):
-        return self.ad_group_name
 
 class Campaign(models.Model):
-    campaign_name = models.CharField(max_length=200, default='')
-    
-    adGroup = models.ForeignKey(AdGroup)
-    
-    def __str__(self):
-        return self.campaign_name
+     campaign_name = models.CharField(max_length=200, default='')
+ 
+     def __str__(self):
+         return self.campaign_name
+ 
+class AdGroup(models.Model):
+     ad_group_name = models.CharField(max_length=200,default='')
+     campaign_name = models.CharField(max_length=200,default='')
+ 
+     campaign = models.ForeignKey(Campaign)
+     def __str__(self):
+         return self.ad_group_name
+         
+class Keyword(models.Model):
+     keyword_id = models.IntegerField(default=0)
+     keyword_placement = models.CharField(max_length=200,default='')
+     clicks = models.IntegerField(default = 0)
+     impressions = models.IntegerField(default = 0)
+     cost = models.IntegerField(default = 0)
+     adgroup = models.ForeignKey(AdGroup)
+     def __str__(self):
+         return self.keyword_placement
