90ea891e6e16996d695ba0f9d1ec0e3190a2fde4
bluej-21 <jys023@ucsd.edu>
2015-08-28 12:25:50 -0700

    added index

--- adwords/urls.py
+++ adwords/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^report/', include('report.urls')),
+    url(r'^index/', include('report.urls')),
 ]
 
