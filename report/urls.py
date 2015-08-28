136e501286dce7c6cb5c2e06bfdc487818c534f3
Zachary <Zacharygking@g.ucla.edu>
2015-08-28 11:27:32 -0700

    URLS/Templates

--- report/urls.py
+++ report/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
 
     url(r'^$', views.index, name='index'),
     url(r'^(?P<campaign_id>[0-9]+)', views.result, name='result'),
-    url(r'generate/', views.main, name='main'),
+    url(r'^generate/', views.main, name='main'),
     url(r'^campaigns/', views.campaigns, name='campaigns'),
     #url(r'campaigns/(?P<campaign_id>[0-9]+)', views.cdetail, name = 'cdetial'),
     ]
