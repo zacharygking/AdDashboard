cc4b98235d39c968e5e704a8d29a035392843acf
bluej-21 <jys023@ucsd.edu>
2015-08-28 12:35:40 -0700

    Update views.py

--- report/views.py
+++ report/views.py
@@ -2,11 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 def index(request):
-<<<<<<< HEAD
-=======
 	return render(request, 'report/index.html')
->>>>>>> origin/master
-
 #!/usr/bin/python
 
 #Google copyright
