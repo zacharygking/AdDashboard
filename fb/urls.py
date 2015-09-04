cfba0477a8102a26a1342c9bcaeaf50b7a84a892
Zachary <Zacharygking@g.ucla.edu>
2015-09-04 10:46:05 -0700

    facebook views

--- fb/urls.py
+++ fb/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
 
     url(r'^$', views.index, name='index'),
+    url(r'^generate/', views.get_report, name='generate'),
 
 ]
--- fb/views.py
+++ fb/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
 def index(request):
-  return HttpResponse("Jae song is bad af")
+	return HttpResponse('jae')
+def get_report(request):
+	return HttpResponse('song')
+
+
 # Create your views here.
