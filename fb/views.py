0ba0a69c5722ce45dc8b1283c1de23bce054e00b
bluej-21 <jys023@ucsd.edu>
2015-09-04 11:09:58 -0700

    Update views.py

--- fb/views.py
+++ fb/views.py
@@ -7,58 +7,52 @@ from facebookads.objects import AdCampaign
 import json
 import ast
 
-
-
 def index(request):
 	return HttpResponse('jsung')
 
 def get_report(request):
 
-  #check if user is authenticated
-  if not request.user.is_authenticated():
-	  return render(request, 'account/login.html')	
+	#check if user is authenticated
+	if not request.user.is_authenticated():
+		return render(request, 'account/login.html')	
 
-  #setting the user information
-  my_app_id = '1604519246476149'
-  my_app_secret = '5a93aee73f1d2856dd542f53e268e483'
-  current_user = SocialAccount.objects.get(user=request.user)
-  current_token = SocialToken.objects.get(account=current_user)
-  my_access_token = current_token.token
+	#setting the user information
+	my_app_id = '1604519246476149'
+	my_app_secret = '5a93aee73f1d2856dd542f53e268e483'
+	current_user = SocialAccount.objects.get(user=request.user)
+	current_token = SocialToken.objects.get(account=current_user)
+	my_access_token = current_token.token
   
-  FacebookAdsApi.init(my_app_id, my_app_secret, my_access_token)
-  me = objects.AdUser(fbid='me')
-  my_accounts = list(me.get_ad_accounts())
+	FacebookAdsApi.init(my_app_id, my_app_secret, my_access_token)
+	me = objects.AdUser(fbid='me')
+	my_accounts = list(me.get_ad_accounts())
   
-  if len(my_accounts) == 0:
-  	return HttpResponse("no Ad Accounts Exist")
-  for current_account in my_accounts:
-  	ad_campaigns = current_account.get_ad_campaigns()
-	  
-	if len(ad_campaigns) == 0:
-		return HttpResponse("no Campaigns Exist")
+	if len(my_accounts) == 0:
+		return HttpResponse("no Ad Accounts Exist")
   
-  	for current_campaign in ad_campaigns:
-  		current_campaign.remote_read(fields=[
-  			AdCampaign.Field.name,
+	for current_account in my_accounts:
+		ad_campaigns = current_account.get_ad_campaigns()
+		if len(ad_campaigns) == 0:
+			return HttpResponse("no Campaigns Exist")
+		
+		for current_campaign in ad_campaigns:
+			current_campaign.remote_read(fields=[
+				AdCampaign.Field.name,
     			AdCampaign.Field.objective,
     			AdCampaign.Field.status,
     			AdCampaign.Field.id,
     		])
-    		
-    		fields = {
+			fields = {
     			'account_id',
     			'impressions',
     			'clicks',
     			'cpc'
     		}
-    
-    		print(current_campaign[AdCampaign.Field.name])
-    		data = str(current_campaign.get_insights(fields=fields))
-    		data = '['+data[12:]
-    		print(data)
-    
-    		ast.literal_eval(data)
-    
-    		json_string = json.dumps(data)
-    		parsed_data = json.loads(data)
-    		print(parsed_data[0]['clicks'])
+			print(current_campaign[AdCampaign.Field.name])
+			data = str(current_campaign.get_insights(fields=fields))
+			data = '['+data[12:]
+			print(data)
+			ast.literal_eval(data)
+			json_string = json.dumps(data)
+			parsed_data = json.loads(data)
+			print(parsed_data[0]['clicks'])
