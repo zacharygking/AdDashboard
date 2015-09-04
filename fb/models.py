8f3e4b62db3e5aba34dd44cd97892bb382b50557
bluej-21 <jys023@ucsd.edu>
2015-09-04 15:06:18 -0700

    Update models.py

--- fb/models.py
+++ fb/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Account(models.Model):
+	accid = models.CharField(max_length=200,default='')
+	
+	def __str__(self):
+		return self.accid
+		
+class Campaign(models.Model):
+	name = models.CharField(max_length=200, default='')
+	camid = models.CharField(max_length=200, default='')
+	status = models.CharField(max_length=200, default='')
+	clicks = models.IntegerField(default=0)
+	cpc = models.FloatField(default=0)
+	impressions = models.IntegerField(default=0)
+	account = models.ForeignKey(Account)
+	
+	def __str__(self):
+		return self.name
