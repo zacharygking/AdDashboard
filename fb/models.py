8fbbc4f31db3fb1a78985bbd47a2f8ba271048b8
bluej-21 <jys023@ucsd.edu>
2015-09-09 15:59:34 -0700

    Update models.py

--- fb/models.py
+++ fb/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Account(models.Model):
 	account_name = models.CharField(max_length=200, default='')
 	account_id = models.CharField(max_length=200,default='')
+	account_cost = models.FloatField(default=0)
 	
 	def __str__(self):
 		return self.account_name
