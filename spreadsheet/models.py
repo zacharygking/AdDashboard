e9101874ffc837d6951f63aca1581066dbdea24d
bluej-21 <jys023@ucsd.edu>
2015-09-10 12:10:48 -0700

    Update models.py

--- spreadsheet/models.py
+++ spreadsheet/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Metric(models.Model):
+class Source(models.Model):
 	name = models.CharField(max_length=200,default='')
 	clicks = models.IntegerField(default=0)
 	impressions = models.IntegerField(default=0)
