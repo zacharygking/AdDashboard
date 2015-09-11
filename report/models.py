8f00e85eb988e01bdd8c48fc249eb52c93f9c824
bluej-21 <jys023@ucsd.edu>
2015-09-11 11:21:04 -0700

    Update models.py

--- report/models.py
+++ report/models.py
@@ -1,31 +1,29 @@
 from django.db import models
 from datetime import datetime
 
-class Account(models.Model):
-    account_name = models.CharField(max_length=200,default='')
-    customer_id = models.CharField(max_length=12,default = '000-000-0000')
-
-    def __str__(self):
-        return self.account_name
-
-class Campaign(models.Model):
+class GoogleCampaign(models.Model):
      campaign_name = models.CharField(max_length=200, default='')
      def __str__(self):
          return self.campaign_name
  
-class AdGroup(models.Model):
+class GoogleAdGroup(models.Model):
      ad_group_name = models.CharField(max_length=200,default='')
      campaign_name = models.CharField(max_length=200,default='')
-     campaign = models.ForeignKey(Campaign)
+     campaign = models.ForeignKey(GoogleCampaign)
      def __str__(self):
          return self.ad_group_name
          
-class Keyword(models.Model):
+class GoogleKeyword(models.Model):
      keyword_id = models.BigIntegerField(default=0)
      keyword_placement = models.CharField(max_length=200,default='')
      clicks = models.BigIntegerField(default = 0)
      impressions = models.BigIntegerField(default = 0)
      cost = models.FloatField(default = 0.0)
-     adgroup = models.ForeignKey(AdGroup)
+     adgroup = models.ForeignKey(GoogleAdGroup)
      def __str__(self):
          return self.keyword_placement
+		 
+
+
+		 
+
