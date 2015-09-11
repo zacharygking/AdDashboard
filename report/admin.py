2cd44fdc12fc399a21cee23b384375c4dd983834
bluej-21 <jys023@ucsd.edu>
2015-09-11 12:46:28 -0700

    Update admin.py

--- report/admin.py
+++ report/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import Report
+from .models import Report, GoogleCampaign, GoogleAdGroup, GoogleKeyword, FacebookCampaign, FacebookAccount
 
 admin.site.register(Report)
+admin.site.register(GoogleCampaign)
+admin.site.register(GoogleAdGroup)
+admin.site.register(GoogleKeyword)
+admin.site.register(FacebookCampaign)
+admin.site.register(FacebookAccount)
