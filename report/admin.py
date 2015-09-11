e7ce9b73b5e26865bb4dc0bb96729a62a3fc4b79
bluej-21 <jys023@ucsd.edu>
2015-09-11 11:21:19 -0700

    Update admin.py

--- report/admin.py
+++ report/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Campaign, AdGroup, Keyword
+from .models import GoogleCampaign, GoogleAdGroup, GoogleKeyword
 
-admin.site.register(Campaign)
-admin.site.register(AdGroup)
-admin.site.register(Keyword)
-# Register your models here.
+admin.site.register(GoogleCampaign)
+admin.site.register(GoogleAdGroup)
+admin.site.register(GoogleKeyword)
