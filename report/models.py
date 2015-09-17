e77dfd5e2b1dca9746bc742953204e9059ba166e
bluej-21 <jys023@ucsd.edu>
2015-09-17 10:30:05 -0700

    Update models.py

--- report/models.py
+++ report/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from datetime import datetime
 
 class Report(models.Model):
@@ -6,11 +7,18 @@ class Report(models.Model):
 	user = models.CharField(max_length=200,default='')
 	
 	def __str__(self):
-		return self.user	
+		return self.user
+		
+class GoogleClient(models.Model):
+	client_id = models.CharField(max_length=200, default='')
+	client_name = models.CharField(max_length=200, default='')
+	user = models.ForeignKey(User, blank=True, null=True)
+	
+	def __str__(self):
+		return self.client_name	
 	
 class GoogleCampaign(models.Model):
 	campaign_name = models.CharField(max_length=200, default='')
-	report = models.ForeignKey(Report, blank=True, null=True)
     
 	def __str__(self):
 		return self.campaign_name
