2e43966c49a1d42329c711c7dd18cbbd4d1368dc
Zachary <Zacharygking@g.ucla.edu>
2015-09-30 14:46:34 -0700

    settings

--- adwords/settings.py
+++ adwords/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/1.8/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.8/ref/settings/
 """
+#!/usr/bin/env python
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
